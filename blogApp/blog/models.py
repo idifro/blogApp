017101558c48278540ca9fdc75c15e39f1b6f646
idifro <idifro@protonmail.com>
2020-03-26 01:05:55 +0530

    Created DB, migration file, user app

--- blogApp/blog/models.py
+++ blogApp/blog/models.py
@@ -1,3 +1,24 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+
+class Post(models.Model):
+    """ This Class is an ORM for the DB Model,
+    This model name is posts and it has a title,
+    content of the post, date_posted which is the acutal
+    date when this object is called upon and finally User.
+
+    Since Django handles users and authentication for us, we
+    import the same to here, the on_delete method is to delete
+    all posts of the user when the user gets deleted
+    """
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
--- blogApp/blog/views.py
+++ blogApp/blog/views.py
@@ -1,26 +1,12 @@
 from django.shortcuts import render
+from .models import Post
 from django.http import HttpResponse
 # Create your views here.
-
-posts = [
-    {
-        'author': 'Harsha',
-        'title': 'Blog post 1',
-        'content': 'This is a test blog post',
-        'date_posted': 'March 25, 2020'
-    },
-    {
-        'author': 'Anu',
-        'title': 'Blog post 2',
-        'content': 'This is a second test blog post',
-        'date_posted': 'March 26, 2020'
-    }
-]
 
 
 def home(request):
     context = {
-        'posts': posts
+        'posts': Post.objects.all()
     }
     return render(request, 'blog/home.html', context)
 
