e33d80504284e20e02d9e480b750cc9ad6e5dbf8
idifro <idifro@protonmail.com>
2020-03-27 00:05:01 +0530

    Now Media Files Loaded through Amazon S3

--- blogApp/blogApp/settings.py
+++ blogApp/blogApp/settings.py
@@ -31,15 +31,16 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'blog.apps.BlogConfig',
     'users.apps.UsersConfig',
     'crispy_forms',
-    'blog.apps.BlogConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'storages'
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,19 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'vardhan671@gmail.com'
+EMAIL_HOST_PASSWORD = 'pdggqyvvhtvtcyjd'
+
+AWS_ACCESS_KEY_ID = 'AKIA4KWMUPY6U22UDTP6'
+AWS_SECRET_ACCESS_KEY = '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'
+AWS_STORAGE_BUCKET_NAME = 'django-blogapp-media-files'
+AWS_S3_REGION_NAME = 'us-east-2'
+
+AWS_S3_FILE_OVERWRITE = False
+AWS_DEFAULT_ACL = None
+DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
